e0e9f5eb4605c61951923de538cb21e41d199d29
Felix Armenta <62727466+FelixH316@users.noreply.github.com>
2025-03-13 18:31:05 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+/*
+AUTHOR:     FELIX ARMENTA
+DATE:       2025-03-13
+PROJECT:    SECRET FRIEND
+*/
+
+let friends_list = []
+
+function agregarAmigo()
+{
+    let friend = document.getElementById("amigo").value;
+
+    if (friend === "" || friend === " ")
+    {
+        alert("Please, insert a name");
+    }
+    else
+    {
+        friends_list.push(friend);
+        cleanField("amigo");
+    }
+}
+
+function cleanField(id)
+{
+    document.querySelector(`#${id}`).value = "";
+    return 0;
+}
+
+function assignTextElement(element, text)
+{
+    let titulo = document.querySelector(element);
+    titulo.innerHTML = text;
+    return 0;
+}
